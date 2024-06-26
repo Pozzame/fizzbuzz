fd3b94003b0ff210d3707df510250e13e2a381a7
Pozzame <pozzame@gmail.com>
2024-06-26 12:28:23 +0200

    Sleep

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿for (int i = 1; i < 100; i++)
+﻿for (int i = 1; i <= 100; i++)
 {
     Console.Write(i + "\t");
     if (i % 3 == 0) Console.Write("fizz");
     if (i % 5 == 0) Console.Write("buzz");
     Console.Write("\n");
+    Thread.Sleep(100);
 }
